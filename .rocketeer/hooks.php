41388e650366456ee6b1680e2219ecb36981f100
postbox433@gmail.com <postbox433@gmail.com>
2016-04-25 18:33:48 +0500

    fix

--- .rocketeer/hooks.php
+++ .rocketeer/hooks.php
@@ -17,20 +17,63 @@ $setComposerAsExecutable = function ($task) {
     $task->runForCurrentRelease('chmod 0770 composer.phar');
 };
 
-$addSshToKnownHosts = function ($task) {
+$addGitToKnownHosts = function ($task) {
     $repo = $task->rocketeer->getOption('scm.repository');
     if (!$repo) return null;
     $arRepo = parse_url($repo);
     if (empty($arRepo['scheme']) || $arRepo['scheme'] !== 'ssh') return null;
-    $hostname = $arRepo['host'] . (!empty($arRepo['port']) ? ":{$arRepo['port']}" : '');
-    $removeCommand = "ssh-keygen -R {$hostname}";
-    $scanCommand = "ssh-keyscan -H {$hostname} >> ~/.ssh/known_hosts";
-    $task->runForCurrentRelease($removeCommand);
-    $task->runForCurrentRelease($scanCommand);
+    $hostname = $arRepo['host'];
+    $task->runForCurrentRelease([
+        "ssh-keygen -R {$hostname}",
+        "ssh-keyscan -t rsa" . (!empty($arRepo['port']) ? " -p{$arRepo['port']}" : '') . " -H {$hostname} >> ~/.ssh/known_hosts"
+    ]);
+};
+
+$createOrShowSshKey = function ($task) {
+    $isKeyExists = trim($task->runRaw('[ -f ~/.ssh/id_rsa.pub ] && echo 1')) === '1';
+    if (!$isKeyExists) {
+        $task->runRaw('ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa');
+    }
+    $key = $task->runRaw('cat ~/.ssh/id_rsa.pub');
+    $task->command->info('Hosting ssh key is: ' . $key);
+    file_put_contents(__DIR__ . '/logs/hosting_key', $key);
+};
+
+$autoCreateShared = function ($task) {
+    $basePath = $task->paths->getHomeFolder() . '/shared';
+    $shareds = $task->rocketeer->getOption('remote.shared');
+    //folders
+    foreach ($shareds as $object) {
+        $arObject = pathinfo($object);
+        if (!empty($arObject['extension'])) $object = $arObject['dirname'];
+        $arPath = explode('/', trim($object, "/ \t\n\r\0\x0B"));
+        $checkedPath = '';
+        foreach ($arPath as $chain) {
+            $fullPath = "{$basePath}{$checkedPath}/{$chain}";
+            $isFolderExists = trim($task->runRaw('[ -d \'' . $fullPath . '\' ] && echo 1')) === '1';
+            if (!$isFolderExists) {
+                $task->run("mkdir '{$fullPath}'");
+            }
+            $checkedPath .= "/{$chain}";
+        }
+    }
+    //files
+    foreach ($shareds as $object) {
+        $arObject = pathinfo($object);
+        if (empty($arObject['extension'])) continue;
+        $object = trim($object, "/ \t\n\r\0\x0B");
+        $fullPath = "{$basePath}/{$object}";
+        if (trim($task->runRaw('[ -f \'' . $fullPath . '\' ] && echo 1')) === '1') continue;
+        $local = __DIR__ . '/../examples/' . $arObject['basename'];
+        $task->run("touch '{$fullPath}'");
+        if (file_exists($local)) {
+            $content = file_get_contents($local);
+            $task->runRaw('echo "' . addcslashes($content, '"') . '" > "'. $fullPath . '"');
+        }
+    }
 };
 
 return [
-
     // Tasks
     //
     // Here you can define in the `before` and `after` array, Tasks to execute
@@ -42,7 +85,6 @@ return [
     // to Rocketeer. Those will then be available in the command line
     // with all the other tasks
     //////////////////////////////////////////////////////////////////////
-
     // Tasks to execute before the core Rocketeer Tasks
     'before' => [
         'setup'   => [],
@@ -52,11 +94,12 @@ return [
             $setComposerAsExecutable,
         ],
     ],
-
     // Tasks to execute after the core Rocketeer Tasks
     'after'  => [
         'setup'   => [
-            $addSshToKnownHosts,
+            $autoCreateShared,
+            $addGitToKnownHosts,
+            $createOrShowSshKey,
         ],
         'deploy'  => [
             $cleanRockteerData,
@@ -67,8 +110,6 @@ return [
             $cleanCache,
         ],
     ],
-
     // Custom Tasks to register with Rocketeer
     'custom' => [],
-
 ];
