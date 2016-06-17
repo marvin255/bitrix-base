09683d71ba8602164f5c596314efc35193d3b150
postbox433@gmail.com <postbox433@gmail.com>
2016-06-17 11:47:59 +0500

    fix

--- .rocketeer/hooks.php
+++ .rocketeer/hooks.php
@@ -1,80 +1,8 @@
 <?php
 
-$cleanRockteerData = function ($task) {
-    $task->command->info('Remove rocketeer info files');
-    $task->runForCurrentRelease('rm -Rf .rocketeer');
-    $task->runForCurrentRelease('rm -Rf rockteer.phar');
-};
-
-$cleanCache = function ($task) {
-    $task->command->info('Clean cache');
-    $task->runForCurrentRelease('rm -Rf web/bitrix/cache/*');
-    $task->runForCurrentRelease('rm -Rf web/bitrix/managed_cache/*');
-};
-
-$setComposerAsExecutable = function ($task) {
-    $task->command->info('Make composer.phar executable');
-    $task->runForCurrentRelease('chmod 0770 composer.phar');
-};
-
-$addGitToKnownHosts = function ($task) {
-    $repo = $task->rocketeer->getOption('scm.repository');
-    if (!$repo) return null;
-    $arRepo = parse_url($repo);
-    if (empty($arRepo['scheme']) || $arRepo['scheme'] !== 'ssh') return null;
-    $isKnownHostsExists = trim($task->runRaw('[ -f ~/.ssh/known_hosts ] && echo 1')) === '1';
-    if (!$isKnownHostsExists) {
-        $task->run("touch ~/.ssh/known_hosts");
-    }
-    $hostname = $arRepo['host'];
-    $task->runForCurrentRelease([
-        "ssh-keygen -R {$hostname}",
-        "ssh-keyscan -t rsa" . (!empty($arRepo['port']) ? " -p{$arRepo['port']}" : '') . " -H {$hostname} >> ~/.ssh/known_hosts"
-    ]);
-};
-
-$createOrShowSshKey = function ($task) {
-    $isKeyExists = trim($task->runRaw('[ -f ~/.ssh/id_rsa.pub ] && echo 1')) === '1';
-    if (!$isKeyExists) {
-        $task->runRaw('ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa');
-    }
-    $key = $task->runRaw('cat ~/.ssh/id_rsa.pub');
-    $task->command->info('Hosting ssh key is: ' . $key);
-};
-
-$autoCreateShared = function ($task) {
-    $basePath = $task->paths->getHomeFolder() . '/shared';
-    $shareds = $task->rocketeer->getOption('remote.shared');
-    //folders
-    foreach ($shareds as $object) {
-        $arObject = pathinfo($object);
-        if (!empty($arObject['extension'])) $object = $arObject['dirname'];
-        $arPath = explode('/', trim($object, "/ \t\n\r\0\x0B"));
-        $checkedPath = '';
-        foreach ($arPath as $chain) {
-            $fullPath = "{$basePath}{$checkedPath}/{$chain}";
-            $isFolderExists = trim($task->runRaw('[ -d \'' . $fullPath . '\' ] && echo 1')) === '1';
-            if (!$isFolderExists) {
-                $task->run("mkdir '{$fullPath}'");
-            }
-            $checkedPath .= "/{$chain}";
-        }
-    }
-    //files
-    foreach ($shareds as $object) {
-        $arObject = pathinfo($object);
-        if (empty($arObject['extension'])) continue;
-        $object = trim($object, "/ \t\n\r\0\x0B");
-        $fullPath = "{$basePath}/{$object}";
-        if (trim($task->runRaw('[ -f \'' . $fullPath . '\' ] && echo 1')) === '1') continue;
-        $local = __DIR__ . '/../examples/' . $arObject['basename'];
-        $task->run("touch '{$fullPath}'");
-        if (file_exists($local)) {
-            $content = file_get_contents($local);
-            $task->runRaw('echo "' . addcslashes($content, '"$') . '" > "'. $fullPath . '"');
-        }
-    }
-};
+//регистрируем события. специфичные для Creative
+require_once __DIR__ . '/../vendor/autoload.php';
+\bxrocketeer\Registrator::events();
 
 return [
     // Tasks
@@ -93,25 +21,14 @@ return [
         'setup'   => [],
         'deploy'  => [],
         'cleanup' => [],
-        'dependencies' => [
-            $setComposerAsExecutable,
-        ],
+        'dependencies' => [],
     ],
     // Tasks to execute after the core Rocketeer Tasks
     'after'  => [
-        'setup'   => [
-            $autoCreateShared,
-            $addGitToKnownHosts,
-            $createOrShowSshKey,
-        ],
-        'deploy'  => [
-            $cleanRockteerData,
-        ],
+        'setup'   => [],
+        'deploy'  => [],
         'cleanup' => [],
-        'update'  => [
-            $cleanRockteerData,
-            $cleanCache,
-        ],
+        'update'  => [],
     ],
     // Custom Tasks to register with Rocketeer
     'custom' => [],
