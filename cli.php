284d6ea5c2fb16e0ddbe53c4286e63629b55f346
Виталий Акмашев <va@crtweb.ru>
2017-03-10 18:19:43 +0500

    fix for new bxmigrate lib, add item for gitignore

--- cli.php
+++ cli.php
@@ -9,6 +9,7 @@ define('NO_KEEP_STATISTIC', true);
 define('NOT_CHECK_PERMISSIONS', true);
 define('CHK_EVENT', true);
 
+require_once(__DIR__.'/vendor/marvin255/bxmigrate/src/Autoloader.php');
 require $_SERVER['DOCUMENT_ROOT'].'/bitrix/modules/main/include/prolog_before.php';
 
 define('CLI_MIGRATIONS_PATH', __DIR__.'/migrations');
@@ -17,8 +18,8 @@ use Symfony\Component\Console\Application;
 
 $application = new Application();
 
-$application->add(new \marvin255\bxmigrate\cli\SymphonyUp());
-$application->add(new \marvin255\bxmigrate\cli\SymphonyDown());
-$application->add(new \marvin255\bxmigrate\cli\SymphonyCreate());
+$application->add(new \marvin255\bxmigrate\cli\SymphonyUp(CLI_MIGRATIONS_PATH));
+$application->add(new \marvin255\bxmigrate\cli\SymphonyDown(CLI_MIGRATIONS_PATH));
+$application->add(new \marvin255\bxmigrate\cli\SymphonyCreate(CLI_MIGRATIONS_PATH));
 
 $application->run();
