8685d13a4e4a25c1b9f63b55cf64799f809f5e23
Andrew Zhdanovskih <andrew72ru@gmail.com>
2017-12-14 09:43:58 +0500

    Feature/codeception (#2)
    * Add 'codeception/codeception' to composer
    
    * Add tests configuration and classes

---
+++ tests/_bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * User: andrew
+ * Date: 13.12.2017
+ * Time: 17:31
+ */
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+define("NOT_CHECK_PERMISSIONS", true);
+define("NO_AGENT_CHECK", true);
+$GLOBALS["DBType"] = 'mysql';
+$_SERVER["DOCUMENT_ROOT"] = dirname(__DIR__) . '/web';
+
+require_once $_SERVER["DOCUMENT_ROOT"] . '/bitrix/modules/main/include.php';
+
+defined('TEST_ENVIRONMENT') or define('TEST_ENVIRONMENT', true);
+
+global $DB;
+$app = \Bitrix\Main\Application::getInstance();
+$con = $app->getConnection();
+$DB->db_Conn = $con->getResource();
+
+$_SESSION["SESS_AUTH"]["USER_ID"] = 1;
---
+++ tests/acceptance/MainPageCest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class MainPageCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    public function tryToSeeBodyElement(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->seeElement('body');
+    }
+}
