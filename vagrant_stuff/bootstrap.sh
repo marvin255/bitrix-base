b4928bdf3c50796c7f0c3d86ed7f19ecedc1fd94
marvin255 <postbox433@gmail.com>
2018-01-15 15:30:41 +0500

    migrate vagrant to php 5.6

--- vagrant_stuff/bootstrap.sh
+++ vagrant_stuff/bootstrap.sh
@@ -1,22 +1,48 @@
 #!/usr/bin/env bash
 
-# Use single quotes instead of double quotes to make it work with special-character passwords
+# default user and default password
+CURRENT_USER=${SUDO_USER:-$USER}
 PASSWORD='password'
+
+# add php repository
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
 
 # update / upgrade
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# install apache 2.5 and php 5.5
-sudo apt-get install -y apache2
-sudo apt-get install -y php5 php5-gd php5-mcrypt php5-sqlite php5-json php5-curl
-sudo php5enmod mcrypt
-
 # install mysql and give password to installer
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
+
+# config mysql
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "sql-mode=\"\"" >> /etc/mysql/my.cnf
+sudo service mysql restart
+
+# install apache and php
+sudo apt-get install -y php5.6 php5.6-cli php5.6-mysql php5.6-gd php5.6-mcrypt php5.6-sqlite php5.6-json php5.6-curl php5.6-mbstring php5.6-xml apache2 libapache2-mod-php5.6
+sudo phpenmod mcrypt
+
+# config php.ini
+mkdir /var/www/session && chown ${CURRENT_USER}:${CURRENT_USER} /var/www/session
+
+sudo sed -i "s/^\(short_open_tag\).*/\1 = On/" /etc/php/5.6/cli/php.ini
+sudo sed -i "s/^;*\(date\.timezone\).*/\1 = 'Europe\/Moscow'/" /etc/php/5.6/cli/php.ini
+sudo sed -i "s/^;*\(session\.save_path\).*/\1 = '\/var\/www\/session'/" /etc/php/5.6/cli/php.ini
+
+sudo sed -i "s/^\(short_open_tag\).*/\1 = On/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^;*\(session\.save_path\).*/\1 = '\/var\/www\/session'/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^\(post_max_size\).*/\1 = 500M/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^\(upload_max_filesize\).*/\1 = 500M/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^\(error_reporting\).*/\1 = E_ALL/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^\(display_errors\).*/\1 = On/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^\(display_startup_errors\).*/\1 = On/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^\(html_errors\).*/\1 = On/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^;*\(opcache\.revalidate_freq\).*/\1 = 0/" /etc/php/5.6/apache2/php.ini
+sudo sed -i "s/^;*\(date\.timezone\).*/\1 = 'Europe\/Moscow'/" /etc/php/5.6/apache2/php.ini
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
@@ -29,7 +55,7 @@ sudo apt-get -y install phpmyadmin
 
 # setup hosts file
 VHOST=$(cat <<EOF
-User vagrant
+User ${CURRENT_USER}
 <VirtualHost *:80>
     DocumentRoot "/var/www/bitrix-base/web"
     <Directory "/var/www/bitrix-base/web">
@@ -38,24 +64,14 @@ User vagrant
         Options -Indexes
         Require all granted
 
-        php_value short_open_tag 1
-        php_value default_charset utf8
         php_admin_value mbstring.func_overload 2
         php_value mbstring.internal_encoding utf8
-        php_value error_reporting E_ALL
-        php_value display_errors On
-        php_value display_startup_errors On
-        php_value html_errors On
+        php_value default_charset utf-8
     </Directory>
 </VirtualHost>
 EOF
 )
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
-
-# config php.ini
-sudo sed -i "s/^\(short_open_tag\).*/\1 = On/" /etc/php5/cli/php.ini
-sudo sed -i "s/^\(post_max_size\).*/\1 = 500M/" /etc/php5/apache2/php.ini
-sudo sed -i "s/^\(upload_max_filesize\).*/\1 = 500M/" /etc/php5/apache2/php.ini
 
 # enable mod_rewrite
 sudo a2enmod rewrite
