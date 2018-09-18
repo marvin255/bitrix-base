71467b1b3e9b3729d9909618627b573198cc09dd
marvin255 <postbox433@gmail.com>
2018-09-18 17:27:02 +0500

    remove useless includings

--- web/local/php_interface/init.php
+++ web/local/php_interface/init.php
@@ -5,10 +5,3 @@ $composerAutoloader = dirname(dirname(dirname(__DIR__))) . '/vendor/autoload.php
 if (file_exists($composerAutoloader)) {
     require_once $composerAutoloader;
 }
-
-//подключаем общие функции для всего проекта
-require_once __DIR__ . '/include/functions.php';
-//подключаем события
-require_once __DIR__ . '/include/events.php';
-//подключаем агентов
-require_once __DIR__ . '/include/agents.php';
