eea458242153f6e10f826397f55f7396b80cecb4
marvin255 <postbox433@gmail.com>
2018-09-20 01:42:28 +0500

    remove useless installer parts

--- lib/composer/Installer.php
+++ lib/composer/Installer.php
@@ -44,11 +44,11 @@ class Installer
     public static function configureProject($event)
     {
         $projectName = $event->getIO()->askAndValidate(
-            self::textToConsole("Укажите название проекта латиницей:\r\n"),
+            "Enter project name in latin:\r\n",
             function ($value) {
                 if (!preg_match('/^[a-z0-9]+$/i', $value)) {
                     throw new InvalidArgumentException(
-                        self::textToConsole('Укажите название проекта латиницей')
+                        'Only latin symbols and digits are allowed'
                     );
                 }
 
@@ -89,36 +89,5 @@ class Installer
     public static function getRootPath()
     {
         return dirname(dirname(__DIR__));
-    }
-
-    /**
-     * Проблема русских букв.
-     *
-     * @param string $message
-     *
-     * @return string
-     */
-    protected static function textToConsole($message)
-    {
-        if (strncasecmp(PHP_OS, 'WIN', 3) == 0) {
-            return self::translit($message);
-        }
-
-        return $message;
-    }
-
-    /**
-     * Транслитератор
-     *
-     * @param string $str
-     *
-     * @return string
-     */
-    protected static function translit($str)
-    {
-        $rus = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'];
-        $lat = ['A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya'];
-
-        return str_replace($rus, $lat, $str);
     }
 }
